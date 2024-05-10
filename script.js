7bc006eb3dba5b1ef193c5418850a84c37198eed
gloria kao <gkao@ucsd.edu>
2024-05-09 17:53:07 -0700

    imported data good and functional tooltip

--- script.js
+++ script.js
@@ -32,29 +32,76 @@ d3.csv("./data/stacked_data.csv", function(data) {
       .domain(allGroup)
       .range(d3.schemeSet2);
 
+    // Set up scales
+    // let xScale = d3.scaleLinear()
+    //   .domain(d3.extent(data, function (d) { return d.year; }))
+    //   .range([0, width]);
+
+    // let yScale = d3.scaleLinear()
+    //   .domain([0, d3.max(data, function (d) { return d3.max(columns, function (c) { return +d[c]; }); })])
+    //   .range([height, 0]);
+    
     // Add X axis --> it is a date format
     let x = d3.scaleLinear()
       .domain(d3.extent(data, function(d) { return d.year; }))
       .range([ 0, width ]);
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).ticks(7));
+      .call(d3.axisBottom(x).tickFormat(d3.format("d")));
 
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.n; })])
+      .domain([0, d3.max(data, function(d) { return +d.count; })])
       .range([ height, 0 ]);
     svg.append("g")
       .call(d3.axisLeft(y));
+
+    // This allows to find the closest X index of the mouse:
+    var bisect = d3.bisector(function(d) { return d.year; }).left;
+
+    // Create the circle that travels along the curve of chart
+    var focus = svg
+      .append('g')
+      .append('circle')
+        .style("fill", "none")
+        .attr("stroke", "black")
+        .attr('r', 8.5)
+        .style("opacity", 0)
+
+    // Create the text that travels along the curve of chart
+    var focusText = svg
+      .append('g')
+      .append('text')
+        .style("opacity", 0)
+        .attr("text-anchor", "left")
+        .attr("alignment-baseline", "middle")
+
+    // Create a rect on top of the svg area: this rectangle recovers mouse position
+    svg
+      .append('rect')
+      .style("fill", "none")
+      .style("pointer-events", "all")
+      .attr('width', width)
+      .attr('height', height)
+      .on('mouseover', mouseover)
+      .on('mousemove', mousemove)
+      .on('mouseout', mouseout);
+
+
+    // What happens when the mouse move -> show the annotations at the right positions.
+    function mouseover() {
+      focus.style("opacity", 1)
+      focusText.style("opacity",1)
+    }
 
     // Initialize line with first group of the list
     var line = svg
       .append('g')
       .append("path")
-        .datum(data.filter(function(d){return d.name==allGroup[0]}))
+        .datum(data.filter(function(d){return d.words==allGroup[0]}))
         .attr("d", d3.line()
           .x(function(d) { return x(d.year) })
-          .y(function(d) { return y(+d.n) })
+          .y(function(d) { return y(+d.count) })
         )
         .attr("stroke", function(d){ return myColor("valueA") })
         .style("stroke-width", 4)
@@ -64,8 +111,8 @@ d3.csv("./data/stacked_data.csv", function(data) {
     function update(selectedGroup) {
 
       // Create new data with the selection?
-      var dataFilter = data.filter(function(d){return d.name==selectedGroup})
-
+      var dataFilter = data.filter(function(d){return d.words==selectedGroup})
+      
       // Give these new data to update line
       line
           .datum(dataFilter)
@@ -73,9 +120,27 @@ d3.csv("./data/stacked_data.csv", function(data) {
           .duration(1000)
           .attr("d", d3.line()
             .x(function(d) { return x(d.year) })
-            .y(function(d) { return y(+d.n) })
+            .y(function(d) { return y(+d.count) })
           )
           .attr("stroke", function(d){ return myColor(selectedGroup) })
+    }
+
+    function mousemove() {
+      // recover coordinate we need
+      var x0 = x.invert(d3.mouse(this)[0]);
+      var i = bisect(data, x0, 1);
+      selectedData = data[i]
+      focus
+        .attr("cx", x(selectedData.year))
+        .attr("cy", y(selectedData.count))
+      focusText
+        .html("x:" + selectedData.year + "  -  " + "y:" + selectedData.count)
+        .attr("x", x(selectedData.year)+15)
+        .attr("y", y(selectedData.count))
+      }
+    function mouseout() {
+      focus.style("opacity", 0)
+      focusText.style("opacity", 0)
     }
 
     // When the button is changed, run the updateChart function
